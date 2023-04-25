cace22eae2aa04e1032779f396e40b86198c52c4
Tyrol Ema <tyrolcg.ema@gmail.com>
2023-04-26 01:43:10 +0900

    add: add some variables
     add some variables of d3d

--- d3d_sample/d3d_sample/app.cpp
+++ d3d_sample/d3d_sample/app.cpp
@@ -104,7 +104,8 @@ LRESULT CALLBACK App::WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
 }
 
 // main loop
-void App::MainLoop() {
+void App::MainLoop()
+{
 	MSG msg = {};
 	
 	while (WM_QUIT != msg.message) {
@@ -116,15 +117,84 @@ void App::MainLoop() {
 }
 
 // quit
-void App::TermApp() {
+void App::TermApp()
+{
 	TermWnd();
 }
 
-void App::TermWnd() {
-	if (m_hInst != nullptr) {
+void App::TermWnd()
+{
+	if(m_hInst != nullptr)
+	{
 		UnregisterClass(ClassName, m_hInst);
 	}
 
 	m_hInst = nullptr;
 	m_hWnd = nullptr;
+}
+
+// initialize d3d
+bool App::InitD3D()
+{
+	// create device
+	auto hr = D3D12CreateDevice(
+		nullptr,
+		D3D_FEATURE_LEVEL_11_0,
+		IID_PPV_ARGS(&m_pDevice)
+	);
+	if (FAILED(hr)) {
+		return false;
+	}
+
+	// create command queue
+	{
+		D3D12_COMMAND_QUEUE_DESC desc = {};
+		desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+		desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+		desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+		desc.NodeMask = 0;
+
+		hr = m_pDevice->CreateCommandQueue(&desc, IID_PPV_ARGS(&m_pQueue));
+		if (FAILED(hr)) {
+			return false;
+		}
+	}
+
+	// create swap chain
+	{
+		// create DXGI factory
+		IDXGIFactory4* pFactory = nullptr;
+		hr = CreateDXGIFactory1(IID_PPV_ARGS(&pFactory));
+		if (FAILED(hr)) {
+			return false;
+		}
+	}
+
+	// setting for swap-chain
+	{
+		DXGI_SWAP_CHAIN_DESC desc = {};
+		desc.BufferDesc.Width = m_Width;
+		desc.BufferDesc.Height = m_Height;
+		desc.BufferDesc.RefreshRate.Numerator = 60;
+		desc.BufferDesc.RefreshRate.Denominator = 1;
+		desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+		desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.BufferCount = FrameCount;
+		desc.OutputWindow = m_hWnd;
+		desc.Windowed = TRUE;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+		desc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+		// create swap-chain
+		IDXGISwapChain* pSwapChain = nullptr;
+		hr = pFactory->CreateSwapChain(m_pQueue, &desc, &pSwapChain);
+		if (FAILED(hr)) {
+			SafeRelease(pFactory);
+			return false;
+		}
+	}
 }
--- d3d_sample/d3d_sample/app.h
+++ d3d_sample/d3d_sample/app.h
@@ -1,7 +1,12 @@
 #pragma once
 #include <Windows.h>
 #include <cstdint>
+#include <d3d12.h>
+#include <dxgi1_4.h>
 
+// linker
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
 
 class App {
 public : 
@@ -10,16 +15,42 @@ public :
 	void Run();
 
 private:
+	/* private variables */
+	static const uint32_t FrameCount = 2; // frame buffer count
+
 	HINSTANCE m_hInst;
 	HWND m_hWnd;
 	uint32_t m_Width;
 	uint32_t m_Height;
 
+	// variables for d3d
+	ID3D12Device* m_pDevice;
+	ID3D12CommandQueue* m_pQueue;
+	IDXGISwapChain3* m_pSwapChain;
+	ID3D12Resource* m_pColorBuffer[FrameCount];
+	ID3D12CommandAllocator* m_pCmdAllocator[FrameCount];
+	ID3D12GraphicsCommandList* m_pCmdList;
+	ID3D12DescriptorHeap* m_pHeapRTV;
+	ID3D12Fence* m_pFence;
+	HANDLE m_FenceEvent;
+	uint64_t m_FenceCounter[FrameCount];
+	uint32_t m_FrameIndex;
+	D3D12_CPU_DESCRIPTOR_HANDLE m_HandleRTV[FrameCount];
+
+	/* private methods */
 	bool InitApp();
 	void TermApp();
 	bool InitWnd();
 	void TermWnd();
 	void MainLoop();
+
+	// methods for d3d
+	bool InitD3D();
+	void TermD3D();
+	void Render();
+	void WaitGpu();
+	void Present(uint32_t interval);
+
 
 	static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
 };
