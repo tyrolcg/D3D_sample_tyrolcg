e1b9d1a63e40eaecdc63463e95bb51c131caa6f7
Tyrol Ema <tyrolcg.ema@gmail.com>
2023-06-07 23:41:45 +0900

    change: change DrawPerFrame

--- d3d_sample/d3d_sample/myDraw.cpp
+++ d3d_sample/d3d_sample/myDraw.cpp
@@ -2,6 +2,7 @@
 
 void App::DrawPerFrame() {
 	m_RotateAngle += 0.025f;
-	m_CBV[m_FrameIndex].pBuffer->World = DirectX::XMMatrixRotationY(m_RotateAngle);
-	m_CBV[m_FrameIndex].pBuffer->World *= DirectX::XMMatrixRotationX(m_RotateAngle);
+	m_CBV[m_FrameIndex].pBuffer->World = DirectX::XMMatrixRotationX(m_RotateAngle);
+	m_CBV[m_FrameIndex].pBuffer->World += DirectX::XMMatrixRotationY(m_RotateAngle);
+	m_CBV[m_FrameIndex].pBuffer->World *= DirectX::XMMatrixRotationZ(m_RotateAngle);
 }
